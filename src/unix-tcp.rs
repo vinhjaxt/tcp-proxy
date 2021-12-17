23f41849c4c8cc48948dbf7b486f82766711283f
vinhjaxt <you@example.com>
2021-12-18 01:59:27 +0700

    first commit

---
+++ src/unix-tcp.rs
@@ -0,0 +1,61 @@
+//! A proxy that forwards data to another server and forwards that server's
+//! responses back to clients.
+//!
+#![warn(rust_2018_idioms)]
+
+use tokio::io;
+use tokio::io::AsyncWriteExt;
+use tokio::net::{TcpListener, TcpStream};
+use tokio::net::{UnixStream};
+
+use futures::FutureExt;
+use std::env;
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let server_addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "/tmp/unix.sock".to_string());
+    let listen_addr = env::args()
+        .nth(2)
+        .unwrap_or_else(|| "127.0.0.1:8081".to_string());
+
+    println!("Listening on: {}", listen_addr);
+    println!("Proxying to: {}", server_addr);
+
+    let listener = TcpListener::bind(listen_addr).await?;
+
+    while let Ok((inbound, _)) = listener.accept().await {
+        let transfer = transfer(inbound, server_addr.clone()).map(|r| {
+            if let Err(e) = r {
+                println!("Failed to transfer; error={}", e);
+            }
+        });
+
+        tokio::spawn(transfer);
+    }
+
+    Ok(())
+}
+
+async fn transfer(mut inbound: TcpStream, proxy_addr: String) -> Result<(), Box<dyn Error>> {
+    let mut outbound = UnixStream::connect(proxy_addr).await?;
+
+    let (mut ri, mut wi) = inbound.split();
+    let (mut ro, mut wo) = outbound.split();
+
+    let client_to_server = async {
+        io::copy(&mut ri, &mut wo).await?;
+        wo.shutdown().await
+    };
+
+    let server_to_client = async {
+        io::copy(&mut ro, &mut wi).await?;
+        wi.shutdown().await
+    };
+
+    tokio::try_join!(client_to_server, server_to_client)?;
+
+    Ok(())
+}
