5b1cf410d3aaf285096e422d69d583b4150383a9
Vinh Duong <ngoc.lo.dang@gmail.com>
2021-12-22 17:42:17 +0700

    monoio

--- src/tcp-tcp.rs
+++ src/tcp-tcp.rs
@@ -1,19 +1,12 @@
-//! A proxy that forwards data to another server and forwards that server's
-//! responses back to clients.
-//!
-#![warn(rust_2018_idioms)]
-
-use tokio::io;
-use tokio::io::AsyncWriteExt;
-use tokio::net::{TcpListener, TcpStream};
-
-use futures::FutureExt;
 use std::env;
-use std::error::Error;
+use monoio::{
+    io::{AsyncReadRent, AsyncWriteRentExt},
+    net::{TcpListener, TcpStream},
+};
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let server_addr = env::args()
+#[monoio::main(entries = 512, timer_enabled = false)]
+async fn main() {
+    let target_address = env::args()
         .nth(1)
         .unwrap_or_else(|| "127.0.0.1:8080".to_string());
     let listen_addr = env::args()
@@ -21,40 +14,49 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .unwrap_or_else(|| "127.0.0.1:8081".to_string());
 
     println!("Listening on: {}", listen_addr);
-    println!("Proxying to: {}", server_addr);
 
-    let listener = TcpListener::bind(listen_addr).await?;
+    let listener = TcpListener::bind(listen_addr)
+        .unwrap_or_else(|_| panic!("unable to bind"));
 
-    while let Ok((inbound, _)) = listener.accept().await {
-        let transfer = transfer(inbound, server_addr.clone()).map(|r| {
-            if let Err(e) = r {
-                println!("Failed to transfer; error={}", e);
+    println!("Proxying to: {}", target_address);
+    loop {
+        if let Ok((in_conn, _addr)) = listener.accept().await {
+            let out_conn = TcpStream::connect(&target_address).await;
+            if let Ok(out_conn) = out_conn {
+                monoio::spawn(async move {
+                    let _ = monoio::join!(
+                        copy_one_direction(&in_conn, &out_conn),
+                        copy_one_direction(&out_conn, &in_conn),
+                    );
+                    println!("relay finished");
+                });
+            } else {
+                eprintln!("dial outbound connection failed");
             }
-        });
-
-        tokio::spawn(transfer);
+        } else {
+            eprintln!("accept connection failed");
+            return;
+        }
     }
-
-    Ok(())
 }
 
-async fn transfer(mut inbound: TcpStream, proxy_addr: String) -> Result<(), Box<dyn Error>> {
-    let mut outbound = TcpStream::connect(proxy_addr).await?;
+async fn copy_one_direction(from: &TcpStream, to: &TcpStream) -> Result<Vec<u8>, std::io::Error> {
+    let mut buf = Vec::with_capacity(8 * 1024);
+    loop {
+        // read
+        let (res, _buf) = from.read(buf).await;
+        buf = _buf;
+        let res: usize = res?;
+        if res == 0 {
+            return Ok(buf);
+        }
 
-    let (mut ri, mut wi) = inbound.split();
-    let (mut ro, mut wo) = outbound.split();
+        // write all
+        let (res, _buf) = to.write_all(buf).await;
+        buf = _buf;
+        res?;
 
-    let client_to_server = async {
-        io::copy(&mut ri, &mut wo).await?;
-        wo.shutdown().await
-    };
-
-    let server_to_client = async {
-        io::copy(&mut ro, &mut wi).await?;
-        wi.shutdown().await
-    };
-
-    tokio::try_join!(client_to_server, server_to_client)?;
-
-    Ok(())
+        // clear
+        buf.clear();
+    }
 }
--- src/tcp-unix.rs
+++ src/tcp-unix.rs
@@ -1,67 +1,86 @@
-//! A proxy that forwards data to another server and forwards that server's
-//! responses back to clients.
-//!
-#![warn(rust_2018_idioms)]
-
-use tokio::io;
-use tokio::io::AsyncWriteExt;
-use tokio::net::{TcpStream};
-use tokio::net::{UnixListener, UnixStream};
-
-use futures::FutureExt;
 use std::env;
-use std::error::Error;
-use std::fs::{self, Permissions};
+use monoio::{
+    io::{AsyncReadRent, AsyncWriteRentExt},
+    net::{UnixListener, UnixStream, TcpStream},
+};
 use std::os::unix::fs::PermissionsExt;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let server_addr = env::args()
+#[monoio::main(entries = 512, timer_enabled = false)]
+async fn main() {
+    let target_address = env::args()
         .nth(1)
-        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+        .unwrap_or_else(|| "127.0.0.1:8081".to_string());
     let listen_addr = env::args()
         .nth(2)
         .unwrap_or_else(|| "/tmp/unix.sock".to_string());
 
     println!("Listening on: {}", listen_addr);
-    println!("Proxying to: {}", server_addr);
 
-    let _ = fs::remove_file(&listen_addr).is_err();
+    let _ = std::fs::remove_file(&listen_addr).is_err();
+    let listener = UnixListener::bind(&listen_addr)
+        .unwrap_or_else(|_| panic!("unable to bind"));
+    let _ = std::fs::set_permissions(listen_addr, std::fs::Permissions::from_mode(0o777)).is_err();
 
-    let listener = UnixListener::bind(&listen_addr)?;
-
-    fs::set_permissions(listen_addr, Permissions::from_mode(0o777))?;
-
-    while let Ok((inbound, _)) = listener.accept().await {
-        let transfer = transfer(inbound, server_addr.clone()).map(|r| {
-            if let Err(e) = r {
-                println!("Failed to transfer; error={}", e);
+    println!("Proxying to: {}", target_address);
+    loop {
+        if let Ok((in_conn, _addr)) = listener.accept().await {
+            let out_conn = TcpStream::connect(&target_address).await;
+            if let Ok(out_conn) = out_conn {
+                monoio::spawn(async move {
+                    let _ = monoio::join!(
+                      copy_one_direction_unix_tcp(&in_conn, &out_conn),
+                      copy_one_direction_tcp_unix(&out_conn, &in_conn),
+                    );
+                    println!("relay finished");
+                });
+            } else {
+                eprintln!("dial outbound connection failed");
             }
-        });
-
-        tokio::spawn(transfer);
+        } else {
+            eprintln!("accept connection failed");
+            return;
+        }
     }
-
-    Ok(())
 }
 
-async fn transfer(mut inbound: UnixStream, proxy_addr: String) -> Result<(), Box<dyn Error>> {
-    let mut outbound = TcpStream::connect(proxy_addr).await?;
+async fn copy_one_direction_tcp_unix(from: &TcpStream, to: &UnixStream) -> Result<Vec<u8>, std::io::Error> {
+  let mut buf = Vec::with_capacity(8 * 1024);
+  loop {
+      // read
+      let (res, _buf) = from.read(buf).await;
+      buf = _buf;
+      let res: usize = res?;
+      if res == 0 {
+          return Ok(buf);
+      }
 
-    let (mut ri, mut wi) = inbound.split();
-    let (mut ro, mut wo) = outbound.split();
+      // write all
+      let (res, _buf) = to.write_all(buf).await;
+      buf = _buf;
+      res?;
 
-    let client_to_server = async {
-        io::copy(&mut ri, &mut wo).await?;
-        wo.shutdown().await
-    };
+      // clear
+      buf.clear();
+  }
+}
 
-    let server_to_client = async {
-        io::copy(&mut ro, &mut wi).await?;
-        wi.shutdown().await
-    };
+async fn copy_one_direction_unix_tcp(from: &UnixStream, to: &TcpStream) -> Result<Vec<u8>, std::io::Error> {
+  let mut buf = Vec::with_capacity(8 * 1024);
+  loop {
+      // read
+      let (res, _buf) = from.read(buf).await;
+      buf = _buf;
+      let res: usize = res?;
+      if res == 0 {
+          return Ok(buf);
+      }
 
-    tokio::try_join!(client_to_server, server_to_client)?;
+      // write all
+      let (res, _buf) = to.write_all(buf).await;
+      buf = _buf;
+      res?;
 
-    Ok(())
+      // clear
+      buf.clear();
+  }
 }
